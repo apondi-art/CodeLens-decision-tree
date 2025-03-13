c5dea8a8825998aae80d97de9a729f2e82fcd031
somulo <mcomulosammy37@gmail.com>
2025-03-13 16:32:52 +0300

    git commit -m"chore:add outputto the functions

--- internal/algorithm/c45.go
+++ internal/algorithm/c45.go
@@ -2,6 +2,12 @@ package algorithm
 
 import "CodeLens-decision-tree/internal/model"
 
-func BuildTree(dataset *model.Dataset, attributes []*model.Attribute, targetAttr string, depth int, maxDepth int) (*model.Node, error)
-func SelectBestAttribute(dataset *model.Dataset, attributes []*model.Attribute, targetAttr string) (*model.Attribute, float64)
-func MajorityClass(dataset *model.Dataset, targetAttr string) string
+func BuildTree(dataset *model.Dataset, attributes []*model.Attribute, targetAttr string, depth int, maxDepth int) (*model.Node, error) {
+	return &model.Node{}, nil
+}
+func SelectBestAttribute(dataset *model.Dataset, attributes []*model.Attribute, targetAttr string) (*model.Attribute, float64) {
+	return &model.Attribute{}, 0
+}
+func MajorityClass(dataset *model.Dataset, targetAttr string) string {
+	return ""
+}
