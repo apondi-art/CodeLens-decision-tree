ff9cdce51e8388f1e0c2c1b78eb1f2e9f78bed06
skisenge <skisengese@gmail.com>
2025-03-13 20:55:52 +0300

    feat: add majorityclass() implementation

--- internal/algorithm/c45.go
+++ internal/algorithm/c45.go
@@ -5,9 +5,23 @@ import "CodeLens-decision-tree/internal/model"
 func BuildTree(dataset *model.Dataset, attributes []*model.Attribute, targetAttr string, depth int, maxDepth int) (*model.Node, error) {
 	return &model.Node{}, nil
 }
+
 func SelectBestAttribute(dataset *model.Dataset, attributes []*model.Attribute, targetAttr string) (*model.Attribute, float64) {
 	return &model.Attribute{}, 0
 }
+
+// MajorityClass returns the most frequent class in the dataset for the target attribute.
 func MajorityClass(dataset *model.Dataset, targetAttr string) string {
-	return ""
+	classCounts := dataset.CountClassInstances()
+	majorityClass := ""
+	maxCount := 0
+
+	for class, count := range classCounts {
+		if count > maxCount {
+			majorityClass = class
+			maxCount = count
+		}
+	}
+
+	return majorityClass
 }
