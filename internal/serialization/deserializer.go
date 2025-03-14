327002ac9faabdb1ba9dc178e084d6ef4af93ca6
antmusumba <musumbaantony3@gmail.com>
2025-03-15 02:59:52 +0300

    feat: enhance deserialization of decision trees with improved JSON handling and structure

--- internal/serialization/deserializer.go
+++ internal/serialization/deserializer.go
@@ -1,10 +1,10 @@
 package serialization
 
 import (
-	"encoding/json"
-	"os"
-
 	"CodeLens-decision-tree/internal/model"
+	"encoding/json"
+	"fmt"
+	"os"
 )
 
 // DeserializeTree loads a decision tree from a JSON file
@@ -16,55 +16,61 @@ func DeserializeTree(path string) (*model.DecisionTree, error) {
 	}
 
 	// Parse JSON
-	var jsonData map[string]interface{}
-	if err := json.Unmarshal(data, &jsonData); err != nil {
-		return nil, err
-	}
-
-	// Convert JSON to DecisionTree
-	tree := &model.DecisionTree{}
-	tree.Root, err = JSONToNode(jsonData["Root"].(map[string]interface{}))
+	var tree model.DecisionTree
+	type Alias model.DecisionTree
+	err = json.Unmarshal(data, &struct {
+		*Alias
+	}{
+		Alias: (*Alias)(&tree),
+	})
 	if err != nil {
 		return nil, err
 	}
 
-	return tree, nil
+	return &tree, nil
 }
 
-// JSONToNode converts a JSON object to a DecisionTree Node
-func JSONToNode(jsonData map[string]interface{}) (*model.Node, error) {
-	node := &model.Node{
-		Attribute: &model.Attribute{Name: jsonData["Attribute"].(string)},
-		IsLeaf:    jsonData["IsLeaf"].(bool),
+// JSONToTreeNode converts a JSON object to a DecisionTree Node
+func JSONToTreeNode(jsonData map[string]interface{}) (*model.TreeNode, error) {
+	node := &model.TreeNode{}
+
+	if splitAttr, ok := jsonData["SplitAttr"].(string); ok {
+		node.SplitAttr = splitAttr
+	}
+
+	if isLeaf, ok := jsonData["IsLeaf"].(bool); ok {
+		node.IsLeaf = isLeaf
 	}
 
 	if predictedClass, ok := jsonData["PredictedClass"].(string); ok {
 		node.PredictedClass = predictedClass
 	}
 
-	if splitValue, ok := jsonData["SplitValue"].(string); ok {
+	if splitValue, ok := jsonData["SplitValue"].(interface{}); ok {
 		node.SplitValue = splitValue
 	}
 
-	// Recursively populate child nodes
-	childrenMap := make(map[string]*model.Node)
-	if children, ok := jsonData["Children"].(map[string]interface{}); ok {
-		for key, childData := range children {
-			childDataMap, ok := childData.(map[string]interface{})
-			if !ok {
+	if classDistribution, ok := jsonData["ClassDistribution"].(map[string]interface{}); ok {
+		node.ClassDistribution = make(map[string]float64)
+		for k, v := range classDistribution {
+			if floatValue, ok := v.(float64); ok {
+				node.ClassDistribution[k] = floatValue
 			}
-
-			childNode, err2 := JSONToNode(childDataMap)
-			if err2 != nil {
-				return nil, err2
-			}
-			childrenMap[key] = childNode
 		}
 	}
 
-	node.Children = make(map[interface{}]*model.Node)
-	for key, child := range childrenMap {
-		node.Children[key] = child
+	// Recursively populate child nodes
+	node.Children = make(map[string]*model.TreeNode)
+	if children, ok := jsonData["Children"].([]interface{}); ok {
+		for i, childData := range children {
+			if childDataMap, ok := childData.(map[string]interface{}); ok {
+				childNode, err := JSONToTreeNode(childDataMap)
+				if err != nil {
+					return nil, fmt.Errorf("error deserializing child %d: %w", i, err)
+				}
+				node.Children[fmt.Sprintf("%d", i)] = childNode
+			}
+		}
 	}
 
 	return node, nil
